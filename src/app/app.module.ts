181cfd5464f0c6c8e23ebeeb5811859860fb3daa
Patrick <patrickniyogitare28@gmail.com>
2021-02-20 11:10:28 +0200

    chore: configured app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestDirective } from './directives/test.directive';
+import { TestPipe } from './pipes/test.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestDirective,
+    TestPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
