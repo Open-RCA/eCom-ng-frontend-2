d753114ab8a62f441305c258402791df56dbb27c
Landrada <Landrada>
2021-02-26 15:23:34 +0200

    Added some comments in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,25 +13,14 @@ export class NavbarComponent implements OnInit {
   name = "James Bradley";
   //userType = "user";
   icon = "./../../../assets/images/dash_icon.png";
-  dashboardLink = "#";
-  notifications = "#";
-  reports ="#";
-  profile_setting ="#";
-  homelink = "#";
   searchIcon = "./../../../assets/icons/search-2.svg";
   profileIcon = "./../../../assets/icons/profile-3.svg";
-  login = "#";
   heartIcon = "./../../../assets/icons/heart-3.svg";
-  heartlink = "#";
-  cart = "#";
   shoppingBag = "./../../../assets/icons/shopping-bag.svg";
-  homeLink = "#";
-  contactlink = "#";
-  shoplink = "#";
   shoppinglogo = "./../../../assets/icons/shopping-cart.svg"
   fourCatdots = "./../../../assets/icons/category-front-icon.svg";
-  // City Names
-  Categories: any = ['Technology', 'Clothes', 'Accesories', 'Shoes']
+  Categories: any = ['Technology', 'Clothes', 'Accesories', 'Shoes'];
+  //usertype for normal user is 'user' and for admin it's 'Admin'
   static userType: any ="Admin";
   //isAdmin:boolean = false;
   get getUserType(){
